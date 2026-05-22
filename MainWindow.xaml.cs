434bceb2545a1d399e592f3a70db640d9b73bb67
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 15:34:48 +0530

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,15 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using TB_Browser.Core.Services;
-using TB_Browser.UI.Controls;
+using System.Windows;
 
 namespace TB_Browser
 {
-    public sealed partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
-        public MainWindow(TabBar tabBar, AddressBar addressBar, BrowserView browserView, TabService tabService)
+        public MainWindow(TabBar tabBar, AddressBar addressBar, BrowserView browserView)
         {
             InitializeComponent();
-
             TabBarHost.Content = tabBar;
             AddressBarHost.Content = addressBar;
             BrowserHost.Content = browserView;
-
-            // Bind tab changes to browser view
-            tabService.ActiveTabChanged += (s, tab) => browserView.SwitchTo(tab);
         }
     }
 }
