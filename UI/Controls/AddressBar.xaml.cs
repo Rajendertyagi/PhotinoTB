9aa354697dccbaab2be592d7e9deb4e51bcd0732
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 16:56:14 +0530

    Update AddressBar.xaml.cs

--- UI/Controls/AddressBar.xaml.cs
+++ UI/Controls/AddressBar.xaml.cs
@@ -9,32 +9,11 @@ namespace TB_Browser.UI.Controls
     public partial class AddressBar : UserControl
     {
         private readonly IBrowserService _svc;
-
-        public AddressBar(IBrowserService svc)
-        {
-            InitializeComponent();
-            _svc = svc;
-            _svc.UrlChanged += (_, u) => UrlBox.Text = u;
-        }
-
-        private void Back_Click(object s, RoutedEventArgs e) { App.Logger.Debug("AddressBar", "Back"); _svc.GoBack(); }
-        private void Forward_Click(object s, RoutedEventArgs e) { App.Logger.Debug("AddressBar", "Forward"); _svc.GoForward(); }
-        private void Reload_Click(object s, RoutedEventArgs e) { App.Logger.Debug("AddressBar", "Reload"); _svc.Reload(); }
-        
-        private void Go_Click(object s, RoutedEventArgs e)
-        {
-            var url = UrlBox.Text;
-            App.Logger.Info("AddressBar", $"Submit: {url}");
-            _svc.Navigate(url);
-        }
-        private void Url_KeyDown(object s, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                var url = UrlBox.Text;
-                App.Logger.Info("AddressBar", $"Enter: {url}");
-                _svc.Navigate(url);
-            }
-        }
+        public AddressBar(IBrowserService svc) { InitializeComponent(); _svc = svc; _svc.UrlChanged += (_, u) => UrlBox.Text = u; }
+        private void Back_Click(object s, RoutedEventArgs e) { Logger.Debug("UI", "Back"); _svc.GoBack(); }
+        private void Forward_Click(object s, RoutedEventArgs e) { Logger.Debug("UI", "Forward"); _svc.GoForward(); }
+        private void Reload_Click(object s, RoutedEventArgs e) { Logger.Debug("UI", "Reload"); _svc.Reload(); }
+        private void Go_Click(object s, RoutedEventArgs e) { Logger.Info("UI", $"Go: {UrlBox.Text}"); _svc.Navigate(UrlBox.Text); }
+        private void Url_KeyDown(object s, KeyEventArgs e) { if (e.Key == Key.Enter) { Logger.Info("UI", $"Enter: {UrlBox.Text}"); _svc.Navigate(UrlBox.Text); } }
     }
 }
