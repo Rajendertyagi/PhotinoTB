bd0aa00483806c1f511371eb42f1658630f98b59
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 21:34:29 +0530

    Update TabModel.cs

--- Core/Models/TabModel.cs
+++ Core/Models/TabModel.cs
@@ -1,19 +1,8 @@
-using System;
-using System.Collections.Generic;
-using TB_Browser.Core.Models;
+namespace TB_Browser.Core.Models;
 
-namespace TB_Browser.Core.Services;
-
-public interface ITabService
+public class TabModel
 {
-    IReadOnlyList<TabModel> Tabs { get; }
-    TabModel? ActiveTab { get; }
-    event EventHandler<TabModel> ActiveTabChanged;
-    event EventHandler<TabModel> TabAdded;
-    event EventHandler<TabModel> TabRemoved;
-
-    TabModel CreateTab();
-    void CloseTab(int id);
-    void ActivateTab(int id);
-    void UpdateTab(int id, string url, string title);
+    public int Id { get; set; }
+    public string Title { get; set; } = "New Tab";
+    public string Url { get; set; } = "about:blank";
 }
