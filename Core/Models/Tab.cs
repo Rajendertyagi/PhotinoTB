04f59bf537536dd83d789d29c0c0c63360f581df
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 22:59:17 +0530

    Create Tab.cs

---
+++ Core/Models/Tab.cs
@@ -0,0 +1,8 @@
+namespace TB_Browser.Core.Models;
+
+public class Tab
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "New Tab";
+    public string Url { get; set; } = "https://www.google.com";
+}
