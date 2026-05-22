99c64457745b08c9a66966dcfae688b2fdf2e67d
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 22:57:37 +0530

    Delete Core directory

--- Core/Models/TabModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TB_Browser.Core.Models;
-
-public class TabModel
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = "New Tab";
-    public string Url { get; set; } = "about:blank";
-}
