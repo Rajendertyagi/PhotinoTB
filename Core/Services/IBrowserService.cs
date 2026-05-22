64f62c388030d3631ae31a8be35bf34919e0c05b
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 21:27:48 +0530

    Create IBrowserService.cs

---
+++ Core/Services/IBrowserService.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Web.WebView2.Core;
+
+namespace TB_Browser.Core.Services;
+
+public interface IBrowserService
+{
+    ITabService TabService { get; set; }
+    void SetWebView(CoreWebView2 webView);
+    void Navigate(string url);
+    void GoBack();
+    void GoForward();
+    void Reload();
+    event EventHandler<string> UrlChanged;
+    event EventHandler<bool> CanGoBackChanged;
+    event EventHandler<bool> CanGoForwardChanged;
+}
