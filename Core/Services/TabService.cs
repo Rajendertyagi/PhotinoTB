13ebf1f366cba59fd8b0d6e1e89679599ac6b0b0
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 18:45:20 +0530

    Update TabService.cs

--- Core/Services/TabService.cs
+++ Core/Services/TabService.cs
@@ -20,35 +20,66 @@ namespace TB_Browser.Core.Services
 
         public TabModel CreateTab()
         {
+            Logger.Info("Tabs", $"Creating new tab (current count: {_tabs.Count})");
             var t = new TabModel { Id = _nextId++ };
             _tabs.Add(t);
+            Logger.Debug("Tabs", $"Tab #{t.Id} added to list");
             ActivateTab(t.Id);
             TabAdded?.Invoke(this, t);
-            Logger.Info("Tabs", $"Created #{t.Id}");
+            Logger.Info("Tabs", $"Tab #{t.Id} created successfully");
             return t;
         }
 
         public void CloseTab(int id)
         {
+            Logger.Info("Tabs", $"Closing tab #{id}");
             var t = _tabs.FirstOrDefault(x => x.Id == id);
-            if (t == null) return;
+            if (t == null)
+            {
+                Logger.Warning("Tabs", $"Tab #{id} not found");
+                return;
+            }
+            
             _tabs.Remove(t);
             TabRemoved?.Invoke(this, t);
-            Logger.Info("Tabs", $"Closed #{id}");
-            if (_tabs.Count == 0) CreateTab();
-            else if (_active?.Id == id) ActivateTab(_tabs.Last().Id);
+            Logger.Debug("Tabs", $"Tab #{id} removed from list (remaining: {_tabs.Count})");
+            
+            if (_tabs.Count == 0)
+            {
+                Logger.Info("Tabs", "No tabs remaining, creating new tab");
+                CreateTab();
+            }
+            else if (_active?.Id == id)
+            {
+                Logger.Debug("Tabs", "Closed active tab, activating last tab");
+                ActivateTab(_tabs.Last().Id);
+            }
+            
+            Logger.Info("Tabs", $"Tab #{id} closed successfully");
         }
 
         public void ActivateTab(int id)
         {
+            Logger.Debug("Tabs", $"Activating tab #{id}");
             _active = _tabs.FirstOrDefault(x => x.Id == id);
-            ActiveTabChanged?.Invoke(this, _active!);
+            if (_active == null)
+            {
+                Logger.Warning("Tabs", $"Tab #{id} not found for activation");
+                return;
+            }
+            ActiveTabChanged?.Invoke(this, _active);
+            Logger.Info("Tabs", $"Tab #{id} activated (Title: {_active.Title})");
         }
 
         public void UpdateTab(int id, string url, string title)
         {
             var t = _tabs.FirstOrDefault(x => x.Id == id);
-            if (t != null) { t.Url = url; t.Title = title; }
+            if (t != null)
+            {
+                t.Url = url;
+                t.Title = title;
+                Logger.Debug("Tabs", $"Tab #{id} updated: {url}");
+            }
         }
     }
 }
