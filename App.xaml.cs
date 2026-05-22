2f70aa638bd0c37d3f96737ef36fd08c24281ce9
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 16:40:55 +0530

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 using TB_Browser.Core.Logging;
 using TB_Browser.Core.Services;
@@ -7,17 +8,19 @@ namespace TB_Browser
 {
     public partial class App : Application
     {
+        public static ILogger Logger { get; private set; } = null!;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var logger = new FileLogger();
-            logger.Info("App", "Application started");
+            Logger = new FileLogger();
+            Logger.Info("App", "Application started");
 
             try
             {
-                var tabService = new TabService(logger);
-                var browserService = new BrowserService(logger);
+                var tabService = new TabService(Logger);
+                var browserService = new BrowserService(Logger);
                 browserService.TabService = tabService;
 
                 var tabBar = new TabBar(tabService);
@@ -29,11 +32,11 @@ namespace TB_Browser
                 var mainWindow = new MainWindow(tabBar, addressBar, browserView);
                 mainWindow.Show();
 
-                logger.Info("App", "MainWindow shown");
+                Logger.Info("App", "MainWindow shown");
             }
             catch (Exception ex)
             {
-                logger.Critical("App", "Startup failed", ex);
+                Logger.Critical("App", "Startup failed", ex);
                 MessageBox.Show($"Fatal Error: {ex.Message}", "TB Browser", MessageBoxButton.OK, MessageBoxImage.Error);
                 Shutdown(1);
             }
