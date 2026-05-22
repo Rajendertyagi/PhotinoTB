96d85d731004ab705de4e62469a9687ee658f2d0
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-22 21:26:38 +0530

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,43 +1,26 @@
-using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media.Animation;
 using TB_Browser.UI.Controls;
 
-namespace TB_Browser
+namespace TB_Browser;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow(TabBar tabBar, AddressBar addressBar, BrowserView browserView)
     {
-        public MainWindow(TabBar tabBar, AddressBar addressBar, BrowserView browserView)
-        {
-            InitializeComponent();
-            TabBarHost.Content = tabBar; AddressBarHost.Content = addressBar; BrowserHost.Content = browserView;
+        InitializeComponent();
+        TabBarHost.Content = tabBar;
+        AddressBarHost.Content = addressBar;
+        BrowserHost.Content = browserView;
 
-            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, (_, _) => tabBar.CreateNewTab()));
-            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, (_, _) => tabBar.CloseActive()));
-            CommandBindings.Add(new CommandBinding(ApplicationCommands.Properties, (_, _) => addressBar.FocusUrl()));
-            CommandBindings.Add(new CommandBinding(NavigationCommands.Refresh, (_, _) => browserView.Reload()));
-            CommandBindings.Add(new CommandBinding(NavigationCommands.BrowseBack, (_, _) => browserView.GoBack()));
-            CommandBindings.Add(new CommandBinding(NavigationCommands.BrowseForward, (_, _) => browserView.GoForward()));
+        CommandBindings.Add(new CommandBinding(ApplicationCommands.New, (_, _) => tabBar.CreateNewTab()));
+        CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, (_, _) => tabBar.CloseActive()));
+        CommandBindings.Add(new CommandBinding(ApplicationCommands.Refresh, (_, _) => browserView.Reload()));
+    }
 
-            browserView.SetProgressHandler(ShowProgress);
-            browserView.SetStatusHandler(s => StatusText.Text = s);
-        }
-
-        private void ShowProgress(bool isVisible)
-        {
-            LoadProgress.Visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
-            if (isVisible)
-            {
-                LoadProgress.Value = 0;
-                LoadProgress.BeginAnimation(ProgressBar.ValueProperty, new DoubleAnimation(100, TimeSpan.FromSeconds(1)));
-            }
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ButtonState == MouseButtonState.Pressed) DragMove();
-        }
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed) DragMove();
     }
 }
