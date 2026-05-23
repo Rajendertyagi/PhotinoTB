459c31861f21ab94af64f40d84ed211fa5c1461d
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-23 16:05:02 +0530

    Create App.xaml.cs

---
+++ Core/App.xaml.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.UI.Xaml;
+using TB.Core.DI;
+using TB.Data.Services;
+using TB.Infrastructure;
+
+namespace TB.Core;
+
+public partial class App : Application
+{
+    public static IServiceProvider Services { get; private set; } = null!;
+
+    public App()
+    {
+        InitializeComponent();
+        InitializeServices();
+    }
+
+    private void InitializeServices()
+    {
+        var pathResolver = new PathResolver();
+        pathResolver.EnsureDirectories();
+        Services = Container.Register(pathResolver);
+        Services.GetRequiredService<DbInitializer>().Initialize();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        new MainWindow().Activate();
+    }
+}
