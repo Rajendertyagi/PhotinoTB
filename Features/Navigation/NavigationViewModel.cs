5a71bba0bc5d26c4ab72475ccf53d01dd9bfd9f8
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-23 20:36:24 +0530

    Create NavigationViewModel.cs

---
+++ Features/Navigation/NavigationViewModel.cs
@@ -0,0 +1,8 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TB.Features.Navigation;
+
+public partial class NavigationViewModel : ObservableObject
+{
+    [ObservableProperty] private string _addressBarText = string.Empty;
+}
