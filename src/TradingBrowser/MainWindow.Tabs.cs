2c1186fb694ed372ea34aac0e45c1843e2e980c7
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-26 22:01:14 +0530

    Update MainWindow.Tabs.cs

--- src/TradingBrowser/MainWindow.Tabs.cs
+++ src/TradingBrowser/MainWindow.Tabs.cs
@@ -6,6 +6,7 @@ using Microsoft.UI.Xaml.Media;
 using TradingBrowser.ViewModels;
 using TradingBrowser.Controls;
 using System.Linq;
+using System.Collections.Generic;
 using Windows.Foundation;
 
 namespace TradingBrowser;
@@ -15,6 +16,10 @@ public sealed partial class MainWindow
     private void TabListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         if (!_isWebViewInitialized || ViewModel.SelectedTab == null) return;
+        
+        // Prevent MainWebView from reloading if we are just selecting a second tab for tiling
+        if (TabListView.SelectedItems.Count > 1) return;
+
         if (e.RemovedItems.Count > 0 && e.RemovedItems[0] is TabViewModel oldTab) oldTab.Url = MainWebView.CoreWebView2.Source;
         
         var newTab = ViewModel.SelectedTab;
@@ -28,38 +33,61 @@ public sealed partial class MainWindow
 
     private void Tab_ContextRequested(object sender, ContextRequestedEventArgs e)
     {
+        // Get all currently selected tabs
+        var selectedTabs = TabListView.SelectedItems.Cast<TabViewModel>().ToList();
+        
         if (sender is TabItemPresenter tabPresenter && tabPresenter.DataContext is TabViewModel tabVM)
         {
-            var menu = new MenuFlyout();
-            
-            var closeItem = new MenuFlyoutItem { Text = "Close tab" };
-            closeItem.Click += (s, args) => ViewModel.CloseTabCommand.Execute(tabVM);
-            menu.Items.Add(closeItem);
-
-            var closeOtherItem = new MenuFlyoutItem { Text = "Close other tabs" };
-            closeOtherItem.Click += (s, args) => 
+            // If the right-clicked tab isn't in the selection, just use it
+            if (!selectedTabs.Contains(tabVM))
             {
-                var tabsToClose = ViewModel.Tabs.Where(t => t != tabVM).ToList();
-                foreach (var t in tabsToClose) ViewModel.CloseTabCommand.Execute(t);
-            };
-            menu.Items.Add(closeOtherItem);
+                selectedTabs = new List<TabViewModel> { tabVM };
+            }
+        }
 
+        var menu = new MenuFlyout();
+        
+        var closeItem = new MenuFlyoutItem { Text = "Close tab" };
+        closeItem.Click += (s, args) => ViewModel.CloseTabCommand.Execute(selectedTabs.LastOrDefault());
+        menu.Items.Add(closeItem);
+
+        var closeOtherItem = new MenuFlyoutItem { Text = "Close other tabs" };
+        closeOtherItem.Click += (s, args) => 
+        {
+            var tabsToClose = ViewModel.Tabs.Where(t => !selectedTabs.Contains(t)).ToList();
+            foreach (var t in tabsToClose) ViewModel.CloseTabCommand.Execute(t);
+        };
+        menu.Items.Add(closeOtherItem);
+
+        // TILING LOGIC: Show Tile option if 2+ tabs are selected
+        if (selectedTabs.Count >= 2)
+        {
+            var tileItem = new MenuFlyoutItem { Text = $"Tile {selectedTabs.Count} Tabs" };
+            tileItem.Click += (s, args) => 
+            {
+                ViewModel.TileSelectedTabs(selectedTabs);
+                TileTabs(selectedTabs[0], selectedTabs[1]);
+            };
+            menu.Items.Add(tileItem);
+        }
+        else
+        {
             var splitItem = new MenuFlyoutItem { Text = "Split Right" };
             splitItem.Click += (s, args) => SplitPane();
             menu.Items.Add(splitItem);
-
-            menu.SystemBackdrop = new DesktopAcrylicBackdrop();
-
-            if (e.TryGetPosition(tabPresenter, out Point point))
-            {
-                menu.ShowAt(tabPresenter, new FlyoutShowOptions { Position = point });
-            }
-            else
-            {
-                menu.ShowAt(tabPresenter);
-            }
-            e.Handled = true;
         }
+
+        menu.SystemBackdrop = new DesktopAcrylicBackdrop();
+
+        if (e.TryGetPosition(tabPresenter, out Point point))
+        {
+            menu.ShowAt(tabPresenter, new FlyoutShowOptions { Position = point });
+        }
+        else
+        {
+            menu.ShowAt(tabPresenter);
+        }
+        e.Handled = true;
     }
 
     private void Tab_MiddleClicked(object sender, PointerRoutedEventArgs e) 
